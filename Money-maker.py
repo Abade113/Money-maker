f7d3f6bc6c1fbff19130e504294f894a764b23d1
wellpence5 <alvinmuthomi22@gmail.com>
2026-03-09 00:35:13 +0800

    Cool loop Abade has never seen before

--- Money-maker.py
+++ Money-maker.py
@@ -1,1 +1,14 @@
-print("Alvin is a monkey")
+def fizzbuzz(n):
+    num = []
+    for i in range(1, n+1):   
+        if i % 15 == 0:
+            num.append("FizzBuzz")
+        elif i % 5 == 0:
+            num.append("Buzz")
+        elif i % 3 == 0:
+            num.append("Fizz")
+        else:
+            num.append(str(i))  
+    return num
+
+print (fizzbuzz(15))
